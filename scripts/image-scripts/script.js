5fe904b02c8058bafb22b382d0f8cd766e7e0d1c
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-05-28 05:00:39 -0700

    crop image and take about white space on top, bottom, left, and right

--- scripts/image-scripts/script.js
+++ scripts/image-scripts/script.js
@@ -2,7 +2,6 @@ const sharp = require('sharp');
 const fs = require('fs');
 const sizeOf = require('image-size');
 const { joinImages } = require('join-images');
-const { fromPath } = require("pdf2pic");
 const PNG = require('pngjs').PNG;
 const { 
   songsWithPageSeeping, 
@@ -11,106 +10,92 @@ const {
   pageWithThreeSongs 
 } = require('./constants');
 
-function getColumnOfMatrix(matrix, columnIndex) {
 
-  return matrix.map(arr => arr[columnIndex])
+const sfogFolderPath = './Songs-for-our-Generation_2015';
+const croppedFilePath = './croppedSfogImages';
+const finalImagePath = './sfogImages'
+const croppedWidth = 1300;
+const croppedHeight = 1682;
+
+if (fs.existsSync(croppedFilePath)){
+  fs.rmSync(croppedFilePath, { recursive: true, force: true });
+}
+fs.mkdirSync(croppedFilePath);
+
+async function cropImage(imgPath, newFilePath, region) {
+  const cropped = await sharp(imgPath)
+    .extract(region);
+  return cropped.toFile(newFilePath);
 }
 
+function getCropImagePromiseArray() {
+  return fs.readdirSync(sfogFolderPath).map((image, index) => {
+    // don't include the first 16 pages and the last page
+    if (![...Array(16).keys(), 283].includes(index)) {
+      const imagePath = `${sfogFolderPath}/${image}`;
+      // image name doesn't matter because we need to process it more anyways
+      const newFilePath = `${croppedFilePath}/${index}.png`;
+      const imageSize = sizeOf(imagePath);
+      const {
+        topWhitespace,
+        bottomWhitespace,
+        leftWhitespace, 
+        rightWhitespace
+      } = getAmountOfWhitespace(imagePath);
+      const region = {
+        left: leftWhitespace,
+        top: topWhitespace,
+        width: imageSize.width - leftWhitespace - rightWhitespace,
+        height: imageSize.height - topWhitespace - bottomWhitespace
+      };
+      console.log(index)
+      return cropImage(imagePath, newFilePath, region).then(() => console.log('done'));
+    }
+  })
+}
 
+async function main() {
+  const cropPromises = getCropImagePromiseArray();
+  console.log('Start cropping');
+  console.log(await Promise.all(cropPromises));
+  console.log('Finished cropping');
 
-// const specimen1 = "./Songs-for-our-Generation_2015.pdf";
-// const outputDirectory = "./sheesh";
+  // if (fs.existsSync(finalImagePath)) {
+  //   fs.rmSync(finalImagePath, { recursive: true, force: true });
+  // }
+  // fs.mkdirSync(finalImagePath);
+  // let finalImagePromises = [];
+  // let currentIndex = 0;
+  // let currentSong = 1;
+  // const allCroppedImagePaths = fs.readdirSync(croppedFilePath).map((imgpath) => `${croppedFilePath}/${imgpath}`);
 
-
-// const sfogFolderPath = './Songs-for-our-Generation_2015';
-// const croppedFilePath = './croppedSfogImages';
-// const finalImagePath = './sfogImages'
-// const croppedWidth = 1300;
-// const croppedHeight = 1682;
-
-// if (fs.existsSync(croppedFilePath)){
-//   fs.rmSync(croppedFilePath, { recursive: true, force: true });
-// }
-// fs.mkdirSync(croppedFilePath);
-
-// async function cropImage(imgPath, newFilePath) {
-//   const imageSize = sizeOf(imgPath);
-
-//   const cropped = await sharp(imgPath)
-//     .extract({ 
-//       left: (imageSize.width - croppedWidth) / 2,
-//       top: (imageSize.height - croppedHeight) / 2, 
-//       width: croppedWidth, 
-//       height: croppedHeight 
-//     });
-//   return cropped.toFile(newFilePath);
-// }
-
-// function getCropImagePromiseArray() {
-//   return fs.readdirSync(sfogFolderPath).map((image, index) => {
-//     // don't include the first 16 pages and the last page
-//     if (![...Array(16).keys(), 283].includes(index)) {
-//       const imagePath = `${sfogFolderPath}/${image}`;
-//       // image name doesn't matter because we need to process it more anyways
-//       const newFilePath = `${croppedFilePath}/${index}.png`;
-//       return cropImage(imagePath, newFilePath);
-//     }
-//   })
-// }
-
-// // function extract(imgPath, startHeight, imageLength, finalFilePath) {
-// //   const imageSize = sizeOf(imgPath);
-// //   const cropped = await sharp(imgPath)
-// //   .extract({ 
-// //     left: (imageSize.width - croppedWidth) / 2,
-// //     top: (imageSize.height - croppedHeight) / 2, 
-// //     width: croppedWidth, 
-// //     height: croppedHeight 
-// //   });
-// // }
-
-// async function main() {
-//   const cropPromises = getCropImagePromiseArray();
-//   console.log('Start cropping');
-//   await Promise.all(cropPromises);
-//   console.log('Finished cropping');
-
-//   if (fs.existsSync(finalImagePath)) {
-//     fs.rmSync(finalImagePath, { recursive: true, force: true });
-//   }
-//   fs.mkdirSync(finalImagePath);
-//   let finalImagePromises = [];
-//   let currentIndex = 0;
-//   let currentSong = 1;
-//   const allCroppedImagePaths = fs.readdirSync(croppedFilePath).map((imgpath) => `${croppedFilePath}/${imgpath}`);
-
-//   while (currentIndex < allCroppedImagePaths.length) {
-//     let increaseIndex = 1;
-//     let increaseSong = 1;
+  // while (currentIndex < allCroppedImagePaths.length) {
+  //   let increaseIndex = 1;
+  //   let increaseSong = 1;
   
-//     if (songsWithPageSeeping.includes(currentSong)) {
-//       increaseIndex = 2;
-//       increaseSong = 2;
-//     } else if (songWithTwoPages.includes(currentSong)) {
-//       increaseIndex = 2;
-//     } else if (pageWithTwoSongs.includes(currentSong)) {
-//       increaseSong = 2;
-//     } else if (currentSong in pageWithThreeSongs) {
-//       increaseSong = 3;
-//     } else {
-//       const finalSongImage = sharp(allCroppedImagePaths[currentIndex])
-//         .toFile(`${finalImagePath}/SFOG_${String(currentSong).padStart(3, '0')}.png`);
-//       finalImagePromises.push(finalSongImage);
-//     }
-//     currentIndex += increaseIndex;
-//     currentSong += increaseSong;
-//   }
-//   console.log('Starting to crop and merge images')
-//   await Promise.all(finalImagePromises);
-//   console.log('Done')
-// }
+  //   if (songsWithPageSeeping.includes(currentSong)) {
+  //     increaseIndex = 2;
+  //     increaseSong = 2;
+  //   } else if (songWithTwoPages.includes(currentSong)) {
+  //     increaseIndex = 2;
+  //   } else if (pageWithTwoSongs.includes(currentSong)) {
+  //     increaseSong = 2;
+  //   } else if (currentSong in pageWithThreeSongs) {
+  //     increaseSong = 3;
+  //   } else {
+  //     const finalSongImage = sharp(allCroppedImagePaths[currentIndex])
+  //       .toFile(`${finalImagePath}/SFOG_${String(currentSong).padStart(3, '0')}.png`);
+  //     finalImagePromises.push(finalSongImage);
+  //   }
+  //   currentIndex += increaseIndex;
+  //   currentSong += increaseSong;
+  // }
+  // console.log('Starting to crop and merge images')
+  // await Promise.all(finalImagePromises);
+  // console.log('Done')
+}
 
-// main();
+main();
 
 
 
@@ -139,16 +124,17 @@ function getColumnOfMatrix(matrix, columnIndex) {
  */
 function getAmountOfWhitespace(imgPath) {
 
-  let xd = fs.readFileSync(imgPath);
-  const png = PNG.sync.read(xd);
-  const pngData = png.data
-  const booleanPixelArray = []
+  let imgFile = fs.readFileSync(imgPath);
+  const png = PNG.sync.read(imgFile);
+  const pngData = png.data;
+  const booleanPixelArray = [];
   
-  // contruct boolean pixel array depending on if the pixel is white or not
+  // build boolean pixel array depending on if the pixel is white or not
   for (let i = 0; i < pngData.length; i += 4) {
     // skip the fourth value (alpha channel) we don't care about that
-    booleanPixelArray.push([pngData[i], pngData[i + 1], pngData[i + 2]].every((num) => num === 255))
+    booleanPixelArray.push([pngData[i], pngData[i + 1], pngData[i + 2]].every((num) => num === 255));
   }
+
   const pngMatrix = []
   for (let i = 0; i < booleanPixelArray.length; i += png.width) {
     const row = booleanPixelArray.slice(i, i + png.width);
@@ -201,5 +187,6 @@ function getAmountOfWhitespace(imgPath) {
   }
 }
 
-const sheesh = getAmountOfWhitespace('SFOG_010.png');
-console.log(sheesh)
+function getColumnOfMatrix(matrix, columnIndex) {
+  return matrix.map(arr => arr[columnIndex])
+}
